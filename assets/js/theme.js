1c231129d006039147ba6426ec9d88d4313ddc90
马军 <majun@xiwicloud.com>
2024-06-09 21:23:27 +0800

    1

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -28,4 +28,20 @@ document.addEventListener("DOMContentLoaded", function () {
     resetAnimation: true,
   });
   wow.init();
+
+  // 初始化地图
+  var map = new AMap.Map("gaodemap", {
+    zoom: 10, // 缩放级别
+    center: [116.397428, 39.90923], // 地图中心点坐标，这里以北京为例
+    // 其他地图参数...
+  });
+
+  // 创建标注点
+  var marker = new AMap.Marker({
+    position: [116.397428, 39.90923], // 标注点坐标
+    title: "公司名称", // 鼠标悬停时显示的标题
+  });
+
+  // 将标注点添加到地图上
+  map.add(marker);
 });
