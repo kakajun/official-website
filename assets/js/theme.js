51e521b0b2ddea6467086cafd266af619b53f7c9
马军 <majun@xiwicloud.com>
2024-06-09 22:17:51 +0800

    1

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -13,6 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 
+  // var mixer = mixitup("#content", {
+  //   load: {
+  //     filter: ".algorithm", // 设置默认激活的过滤器
+  //   },
+  //   // ... 其他配置
+  // });
   var mixer = mixitup("#content");
 
   /* =====================================================
@@ -31,15 +37,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // 初始化地图
   var map = new AMap.Map("gaodemap", {
-    zoom: 10, // 缩放级别
-    center: [116.397428, 39.90923], // 地图中心点坐标，这里以北京为例
+    zoom: 14, // 缩放级别
+    center: [113.924898, 22.531384], // 地图中心点坐标，这里以北京为例
     // 其他地图参数...
   });
 
   // 创建标注点
   var marker = new AMap.Marker({
-    position: [116.397428, 39.90923], // 标注点坐标
-    title: "公司名称", // 鼠标悬停时显示的标题
+    position: [113.924898, 22.531384], // 标注点坐标
+    title: "深圳市汇择科技有限公司", // 鼠标悬停时显示的标题
   });
 
   // 将标注点添加到地图上
