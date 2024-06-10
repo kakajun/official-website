ba11520d7511008c77f4e538dc058a21abc9c2bc
马军 <majun@xiwicloud.com>
2024-06-10 11:53:11 +0800

    干掉一些不要的

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # official-website
-深圳市汇择科技有限公司官网, 用bootstrap.5.0.0改造而成
+
+深圳市汇择科技有限公司官网, 用 bootstrap.5.0.0 改造而成
+
+## 预览
+
+[官方Demo](https://kakajun.github.io/official-website/)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,110 +1,116 @@
+(function () {
+  //===== Prealoder
 
-(function() {
-//===== Prealoder
+  window.onload = function () {
+    window.setTimeout(fadeout, 500);
+  };
 
-	window.onload = function () {
-		window.setTimeout(fadeout, 500);
-	}
+  function fadeout() {
+    document.querySelector(".preloader").style.opacity = "0";
+    document.querySelector(".preloader").style.display = "none";
+  }
 
-	function fadeout() {
-		document.querySelector('.preloader').style.opacity = '0';
-		document.querySelector('.preloader').style.display = 'none';
-	}
-
-
-    /*=====================================
+  /*=====================================
     Sticky
     ======================================= */
-    window.onscroll = function () {
-        var header_navbar = document.querySelector(".navbar-area");
-        var sticky = header_navbar.offsetTop;
-        var logo = document.querySelector(".navbar-brand img");
+  window.onscroll = function () {
+    var header_navbar = document.querySelector(".navbar-area");
+    var sticky = header_navbar.offsetTop;
+    var logo = document.querySelector(".navbar-brand img");
 
-        if (window.pageYOffset > sticky) {
-            header_navbar.classList.add("sticky");
-            logo.src = 'assets/img/logo/logo-2.svg'
-        } else {
-            header_navbar.classList.remove("sticky");
-            logo.src = 'assets/img/logo/logo.svg'
-        }
+    if (window.pageYOffset > sticky) {
+      header_navbar.classList.add("sticky");
+      logo.src = "assets/img/logo/logo-2.svg";
+    } else {
+      header_navbar.classList.remove("sticky");
+      logo.src = "assets/img/logo/logo.svg";
+    }
 
+    // show or hide the back-top-top button
+    var backToTo = document.querySelector(".scroll-top");
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      backToTo.style.display = "flex";
+    } else {
+      backToTo.style.display = "none";
+    }
+  };
 
+  // Get the navbar
 
-        // show or hide the back-top-top button
-        var backToTo = document.querySelector(".scroll-top");
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            backToTo.style.display = "flex";
-        } else {
-            backToTo.style.display = "none";
-        }
-    };
+  // for menu scroll
+  var pageLink = document.querySelectorAll(".page-scroll");
 
-    // Get the navbar
-
-
-    // for menu scroll 
-    var pageLink = document.querySelectorAll('.page-scroll');
-    
-    pageLink.forEach(elem => {
-        elem.addEventListener('click', e => {
-            e.preventDefault();
-            document.querySelector(elem.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth',
-                offsetTop: 1 - 60,
-            });
-        });
+  pageLink.forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector(elem.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+        offsetTop: 1 - 60,
+      });
+      // var element = document.querySelector(elem.getAttribute("href"));
+      // // 计算滚动到的位置，顶部偏移量
+      // var scrollPosition = element.offsetTop - 50;
+      // // 滚动到元素，同时传入一个选项对象
+      // element.scrollIntoView({
+      //   behavior: "smooth", // 平滑滚动
+      //   block: "start", // 元素的顶部与视口顶部对齐
+      // });
+      // window.scrollTo({
+      //   top: scrollPosition,
+      //   left: 0,
+      //   behavior: "smooth",
+      // });
     });
+  });
 
+  //===== close navbar-collapse when a  clicked
+  let navbarToggler = document.querySelector(".navbar-toggler");
+  var navbarCollapse = document.querySelector(".navbar-collapse");
 
-    //===== close navbar-collapse when a  clicked
-    let navbarToggler = document.querySelector(".navbar-toggler");    
-    var navbarCollapse = document.querySelector(".navbar-collapse");
+  document.querySelectorAll(".page-scroll").forEach((e) =>
+    e.addEventListener("click", () => {
+      navbarToggler.classList.remove("active");
+      navbarCollapse.classList.remove("show");
+    })
+  );
+  navbarToggler.addEventListener("click", function () {
+    navbarToggler.classList.toggle("active");
+  });
 
-    document.querySelectorAll(".page-scroll").forEach(e =>
-        e.addEventListener("click", () => {
-            navbarToggler.classList.remove("active");
-            navbarCollapse.classList.remove('show')
-        })
-    );
-    navbarToggler.addEventListener('click', function() {
-        navbarToggler.classList.toggle("active");
-    }) 
+  // WOW active
+  new WOW().init();
 
-
-	// WOW active
-    new WOW().init();
-
-	//======== tiny slider
-	var slider = new tns({
-		container: '.testimonial-active',
-		slideBy: 'page',
-		autoplay: false,
-		mouseDrag: true,
-        gutter: 0,
+  //======== tiny slider
+  var slider = new tns({
+    container: ".testimonial-active",
+    slideBy: "page",
+    autoplay: false,
+    mouseDrag: true,
+    gutter: 0,
+    items: 1,
+    nav: true,
+    controls: false,
+    responsive: {
+      0: {
         items: 1,
-		nav: true,
-        controls: false,
-        responsive: {
-            0: {
-                items: 1,
-            },
+      },
 
-            992: {
-                items: 2,
-            }
+      992: {
+        items: 2,
+      },
+    },
+  });
 
-        }
-    });
-
-    
-    //====== counter up 
-    var cu = new counterUp({
-        start: 0,
-        duration: 2000,
-        intvalues: true,
-        interval: 100,
-        append: " ",
-    });
-    cu.start();
-   
+  //====== counter up
+  var cu = new counterUp({
+    start: 0,
+    duration: 2000,
+    intvalues: true,
+    interval: 100,
+    append: " ",
+  });
+  cu.start();
 })();
